fea806f014c5a6556ca9d77603c65b848852a74b
hamza-zenkoders <hamzaahmed.zenkoders@gmail.com>
2024-11-25 19:56:26 +0500

    added add and delete functionality

--- App.js
+++ App.js
@@ -1,20 +1,52 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
+import { Button, FlatList, StyleSheet, Text, TextInput, TouchableOpacity, View } from "react-native";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  const [todo, setTodo] = useState([]);
+  const [curr, setCurr] = useState('');
+
+  function pressHandler () {
+    setTodo(prev => [...prev, curr]);
+    setCurr('');
+  }
+
+  function deleteHandler (index) {
+    setTodo(prev => prev.filter((todo, ind) => ind !== index));
+  }
+
+  return <View style={styles.container}>
+      <View style={styles.singleLine}><TextInput value={curr} placeholder="Enter Todo" style={styles.input} onChangeText={setCurr} />
+      <Button title="Add" onPress={pressHandler} />
+      </View>
+      <FlatList 
+        data={todo}
+        renderItem={({item, index}) => (
+          <Text style={styles.item}>{index + 1} : {item} <Button title="Delete" onPress={() => deleteHandler(index)} /> 
+          <Button title="Edit" onPress={() => deleteHandler(index)} /> </Text>
+        )}
+      />
+    </ View>
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    paddingTop: 100,
+    paddingLeft: 20
   },
+  singleLine: {
+    display: 'flex',
+    flexDirection: 'row',
+    gap: 10
+  },
+  item: {
+    marginBlock: 10,
+    display: 'flex',
+    // flexDirection: 'row',
+    // justifyContent: 'space-between',
+    // alignItems:'center',
+    fontWeight: 'bold',
+  }
 });
